1723b1ee1dbcf9b624426a7223f6b7c5246d1291
acer_laptop <2539468796@qq.com>
2024-10-02 16:00:01 +0800

    112 路径总和(即之前回溯path的更改版本,比较简单)

---
+++ leetcode/editor/cn/leetcode_num_112.cpp
@@ -0,0 +1,75 @@
+//import universal *.h
+#include "../../../stdc.h"
+
+using namespace std;
+
+namespace solution112{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// note 在traversal()函数中res一开始没有直接赋值为0, 导致计算上一直有问题
+
+bool traversal(TreeNode* treeNode, vector<int>& path, int targetSum)
+{
+    path.push_back(treeNode->val);
+    // 找到叶子节点
+    if(treeNode->left == nullptr && treeNode->right == nullptr)
+    {
+        int res;
+        for(auto i : path)
+            res += i;
+        if(res == targetSum)
+            return true;
+        else
+            return false;
+    }
+
+    if(treeNode->left != nullptr)
+    {
+        if(traversal(treeNode->left, path, targetSum))
+            return true;
+        path.pop_back();
+    }
+
+    if(treeNode->right != nullptr)
+    {
+        if(traversal(treeNode->right, path, targetSum))
+            return true;
+        path.pop_back();
+    }
+
+    return false;
+}
+
+
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum)
+    {
+        if(root == nullptr) return false;
+        vector<int> path;
+        return traversal(root, path, targetSum);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution112;
+int main() {
+    Solution solution = Solution();
+
+    return 0;
+}
+
