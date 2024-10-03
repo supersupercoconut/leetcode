626005990f069f0106fbc341b69c7bf4ae068f40
acer_laptop <2539468796@qq.com>
2024-10-03 21:35:55 +0800

    617 合并二叉树(简单)

---
+++ leetcode/editor/cn/leetcode_num_617.cpp
@@ -0,0 +1,62 @@
+//import universal *.h
+#include "../../../stdc.h"
+
+using namespace std;
+
+namespace solution617{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+TreeNode* merge(TreeNode* ptr_1, TreeNode* ptr_2)
+{
+    if(ptr_1 == nullptr && ptr_2 == nullptr)
+        return nullptr;
+    else if(ptr_1 == nullptr && ptr_2 != nullptr)
+        return ptr_2;
+    else if(ptr_1 != nullptr && ptr_2 == nullptr)
+        return ptr_1;
+    else
+    {
+        int new_val = ptr_1->val + ptr_2->val;
+        TreeNode* new_node = new TreeNode(new_val);
+        // 给定左右子树
+        TreeNode* left = merge(ptr_1->left, ptr_2->left);
+        TreeNode* right = merge(ptr_1->right, ptr_2->right);
+        new_node->left = left;
+        new_node->right = right;
+        return new_node;
+    }
+
+}
+
+
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2)
+    {
+        // 函数自带异常状态判断功能
+        return merge(root1, root2);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution617;
+int main() {
+    Solution solution = Solution();
+
+    return 0;
+}
+
