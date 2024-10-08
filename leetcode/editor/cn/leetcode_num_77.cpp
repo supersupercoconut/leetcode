580122b6df312e484f5af4bf2a5a51f6a380d102
acer_laptop <2539468796@qq.com>
2024-10-08 17:11:18 +0800

    77 回溯 组合优化

--- leetcode/editor/cn/leetcode_num_77.cpp
+++ leetcode/editor/cn/leetcode_num_77.cpp
@@ -16,12 +16,16 @@ public:
             res.push_back(path);
             return;
         }
-        // 从start_index开始，遍历到n
+        // 从start_index开始，遍历到n | 增加剪枝操作 —— 这样整个算法的时间复杂度为 k* (C_n)^k 与需要遍历二叉树的深度有关(k也是整个问题中定义的k值)
         for(int i = start_index; i <= n; ++i)
         {
-            path.push_back(i);
-            backtracking(n,k,i+1);
-            path.pop_back();    // 增加这里部分的主要作用就是满足下一次循环开始的时候, path去除上一次的影响
+            // 判断 当前剩余元素 + 当前元素量(也就是1) + 已元素量(path.size()) 要大于等于k, 才说明后面还必要继续去遍历元素
+            if( (n - i + path.size() + 1) >= k )
+            {
+                path.push_back(i);
+                backtracking(n,k,i+1);
+                path.pop_back();    // 增加这里部分的主要作用就是满足下一次循环开始的时候, path去除上一次的影响
+            }
         }
     }
 
