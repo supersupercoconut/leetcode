2299711fef60e4a1000ff5b7c947a0b7b68a539d
acer_laptop <2539468796@qq.com>
2024-10-22 22:48:11 +0800

    134 加油站

--- leetcode/editor/cn/leetcode_num_134.cpp
+++ leetcode/editor/cn/leetcode_num_134.cpp
@@ -15,27 +15,48 @@ public:
 
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         /* 暴力方法 */
+//        for(int i = 0; i < gas.size(); ++i)
+//        {
+//            int vel = 0;        // 当前油量
+//            int j = i;
+//            while(1)
+//            {
+//                vel += gas[j];
+//                vel -= cost[j];
+//                // note 一定要注意这个部分，我自己定义的 j=0的写法在clion的编译器上可以通过但是在leetcode的编译器上就不能通过
+//                j = (j + 1) % gas.size();
+//                cout << vel <<' ';
+//                if(vel < 0) break;
+//                if(j == i) return i;
+//            }
+//        }
+//        return -1;
 
+        /* 贪心方法(计算每次加油站的结余量，即代码随想录中的方法) */
+        /*
+         *  (1) count < 0 即start 从 i + 1 开始，说明之前的rest[i]结余不够，一定是i或者i之前一段部分都取值为负数，这时候从前面选择start只会让正数变少，一定不对
+         *  (2) total > 0 相当于保证整个数据里面是会出现解的
+         *  (3) 因为解只会出现一次，这里从 i + 1 开始(之后肯定没有负数，要不然起始位置就不是从这里开始了) 在total大于等于0保证存在解的情况下，这是最可能是解的位置
+         * */
+        int count = 0;
+        int total = 0;
+        int start = 0;
         for(int i = 0; i < gas.size(); ++i)
         {
-            int vel = 0;        // 当前油量
-            int j = i;
-            while(1)
+            count += (gas[i] - cost[i]);
+            total += (gas[i] - cost[i]);
+            // 这里未必只会出现一次 count小于0的情况，但是只要count < 0 就直接让 i 变为 i+1
+            if(count < 0)
             {
-                vel += gas[j];
-//                if( j >= gas.size() ) j = 0;
-                vel -= cost[j];
-//                j++;
-                // note 一定要注意这个部分，我自己定义的 j=0的写法在clion的编译器上可以通过但是在leetcode的编译器上就不能通过
-                j = (j + 1) % gas.size();
-                cout << vel <<' ';
-                if(vel < 0) break;
-                if(j == i) return i;
+                start = i + 1;
+                count = 0;
             }
-            cout << endl;
-//            cout << i << " ";
+
         }
-        return -1;
+        // 判断当前是否有解
+        if(total < 0) return -1;
+        else
+            return start;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
