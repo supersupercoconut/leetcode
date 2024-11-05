6fe316b6f8629ef518850ee79a05c3fa7f32d209
acer_laptop <2539468796@qq.com>
2024-11-05 21:28:43 +0800

    1049 最后一块石头的重量

---
+++ leetcode/editor/cn/leetcode_num_1049.cpp
@@ -0,0 +1,40 @@
+//import universal *.h
+#include "../../../stdc.h"
+using namespace std;
+
+namespace solution1049{
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    // 最后剩余的部分要求最小，说明其无论是什么样的选择方式，只去需寻找最后最小的部分
+    int lastStoneWeightII(vector<int>& stones)
+    {
+        int sum = 0;
+        for(auto i : stones)
+            sum += i;
+
+        int target = sum / 2;
+        vector<int> dp(target+1,0);
+
+        for(int i = 0; i < stones.size(); ++i)
+        {
+            for(int j = target; j >= stones[i]; --j)
+            {
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        return sum - 2*dp[target];
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution1049;
+int main() {
+    Solution solution = Solution();
+
+    return 0;
+}
+
