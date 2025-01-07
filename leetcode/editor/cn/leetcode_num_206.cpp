2c3ac7158a3a4b962acb2cdb962887b1c010565e
supercoconut <supersupercoconut@gmail.com>
2025-01-07 20:33:41 +0800

    leetcode 206 反转链表

---
+++ leetcode/editor/cn/leetcode_num_206.cpp
@@ -0,0 +1,52 @@
+#include "../../../stdc.h"
+
+using namespace std;
+
+
+namespace solution206{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // 给定虚拟的头节点来分析问题
+        if(head == nullptr) return nullptr;
+        ListNode* myHead = new ListNode(0, head);
+        ListNode* pre = myHead;
+        ListNode* cur = head;
+
+        while(cur != nullptr)
+        {
+            ListNode* tmp = cur->next;
+            // 设置的虚拟头节点并不能满足我们的需求，反转之后是不能出现虚拟的头节点的
+            if(pre == myHead)
+                cur->next = nullptr;
+            else
+                cur->next = pre;
+
+            pre = cur;
+            cur = tmp;
+        }
+
+        return pre;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+using namespace solution206;
+int main() {
+    Solution solution = Solution();
+
+    return 0;
+}
