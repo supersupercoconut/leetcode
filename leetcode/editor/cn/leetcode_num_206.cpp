1fc4ec6a34b5f82b1f464c8791035d23a8dd2f61
supercoconut <supersupercoconut@gmail.com>
2025-01-09 11:04:04 +0800

    leetcode 206 反转链表(双指针以及递归方法)

--- leetcode/editor/cn/leetcode_num_206.cpp
+++ leetcode/editor/cn/leetcode_num_206.cpp
@@ -18,30 +18,46 @@ namespace solution206{
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // 给定虚拟的头节点来分析问题
-        if(head == nullptr) return nullptr;
-        ListNode* myHead = new ListNode(0, head);
-        ListNode* pre = myHead;
-        ListNode* cur = head;
+    /*** 双指针方法 ***/
+        // 给定虚拟的头节点来分析问题(这里是否使用虚拟头节点的意义不是很大)
+//        if(head == nullptr) return nullptr;
+//        ListNode* myHead = new ListNode(0, head);
+//        ListNode* pre = myHead;
+//        ListNode* cur = head;
+//
+//        while(cur != nullptr)
+//        {
+//            ListNode* tmp = cur->next;
+//            // 设置的虚拟头节点并不能满足我们的需求，反转之后是不能出现虚拟的头节点的
+//            if(pre == myHead)
+//                cur->next = nullptr;
+//            else
+//                cur->next = pre;
+//
+//            pre = cur;
+//            cur = tmp;
+//        }
+//
+//        delete myHead;
+//        return pre;
 
-        while(cur != nullptr)
+        /*** 递归方法(为避免使用虚拟头节点导致的程序还需一步判断的方法，这里可以直接设置pre一开始为nullptr) ***/
+        return reverse(nullptr, head);
+    }
+
+    ListNode* reverse(ListNode* pre, ListNode* cur)
+    {
+        if(cur == nullptr) return pre;
+        else
         {
             ListNode* tmp = cur->next;
-            // 设置的虚拟头节点并不能满足我们的需求，反转之后是不能出现虚拟的头节点的
-            if(pre == myHead)
-                cur->next = nullptr;
-            else
-                cur->next = pre;
-
-            pre = cur;
-            cur = tmp;
+            cur->next = pre;
+            // 互换位置
+            return reverse(cur, tmp);
         }
-
-        return pre;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
-
 }
 
 using namespace solution206;
