f0196c1b29592791234530877d2a13a49f201630
supercoconut <supersupercoconut@gmail.com>
2025-02-17 19:42:52 +0800

    leetcode 226(二刷)

--- leetcode/editor/cn/leetcode_num_226.cpp
+++ leetcode/editor/cn/leetcode_num_226.cpp
@@ -19,16 +19,27 @@ namespace solution226{
  * };
  */
 
-void traverse(TreeNode* treeNode)
-{
-    // 对于中心节点 互换左右(这里无需判断左右是不是空);
-    TreeNode* temp = treeNode->left;
-    treeNode->left = treeNode->right;
-    treeNode->right = temp;
-    if(treeNode->left) traverse(treeNode->left);
-    if(treeNode->right) traverse(treeNode->right);
+//void traverse(TreeNode* treeNode)
+//{
+//    // 对于中心节点 互换左右(这里无需判断左右是不是空);
+//    TreeNode* temp = treeNode->left;
+//    treeNode->left = treeNode->right;
+//    treeNode->right = temp;
+//    if(treeNode->left) traverse(treeNode->left);
+//    if(treeNode->right) traverse(treeNode->right);
+//
+//}
 
-}
+
+
+
+    void traverse(TreeNode* root)
+    {
+        if(root == nullptr) return;
+        swap(root->left, root->right);
+        traverse(root->left);
+        traverse(root->right);
+    }
 
 class Solution {
 public:
